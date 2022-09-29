db341593b5b5407246c84c81cdf3a396076b62e3
MarcinSkic <marcinskicoficjal@gmail.com>
2022-09-29 12:59:48 +0200

    Create pages structure using Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        raports: './src/pages/raports/raports.js'
+    },
     devtool: 'inline-source-map',
     module: {
         rules: [
 
         ],
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: "Habit Tracker|Home",
+            filename: "index.html",
+            template: "./src/index.html",
+            chunks: ['index'],
+        }),
+        new HtmlWebpackPlugin({
+            title: "Habit Tracker|Raports",
+            filename: "pages/raports.html",
+            template: "./src/pages/raports/site.html",
+            chunks: ['raports'],
+        })
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname,'dist'),
+        clean: true,
     }
 }
