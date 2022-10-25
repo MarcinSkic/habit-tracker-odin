39073a5d89abd904a9de4976aa80a369c572120a
MarcinSkic <marcinskicoficjal@gmail.com>
2022-10-25 10:50:52 +0200

    Create raw positYN dialog

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const DOMController = (function(){
         <a href="/dist/pages/raports.html"><button class="raports">Raports</button></a>`;
     }
 
-    function habitPickerDialog(){
+    function createHabitPickerDialog(){
         const dialogContainer = document.getElementById("pick-habit");
         /*dialogContainer.innerHTML = ``*/
         
@@ -30,49 +30,88 @@ const DOMController = (function(){
         
         const dialog = new A11yDialog(dialogContainer);
         dialog.on('show',generateHabitDialog);
-        const createHabitForm = dialogContainer.querySelector("form");
-
-        createHabitForm.addEventListener('submit',testFormData);
     }
 
     function testFormData(event){
         let formData = new FormData(this);
-        console.log(this);
-        console.log(...formData.entries());
+        const entries = [...formData.entries()];
+        console.log(entries[0]);
+        console.log(...formData.values());
+        console.log(typeof([...formData.entries()]));
         event.preventDefault();
     }
 
-    const habitCreator = (function(){
+    const habitGenerator = (function(){
 
         function generateDialogContent(type){
-            const container = document.createElement("div");
+            const dialog = document.querySelector('#create-habit');
+            const dialogContent = dialog.querySelector('.dialog-content');
+            const form = dialogContent.querySelector("form");
+
+            form.innerHTML = `
+                <input type="hidden" name="type" value="${type}">
+                <input type="color" name="color">
+            `
 
             switch(type){
                 case "positiveYN":
-                    console.log("Sukcesywnie wygenerowano ");
+                    generatePositiveYN(form);
                     break;
                 case "negativeYN":
+                    generateNegativeYN(form);
                     break;
                 case "positiveNumerical":
+                    generatePositiveNumerical(form);
                     break;
                 case "negativeNumerical":
+                    generateNegativeNumerical(form);
                     break;
             }
+
+            form.innerHTML += `
+                <div class="frequency">
+                    <label for="everyday">Everyday</label>
+                    <input type="radio" name="frequency" id="everyday" value="everyday">
+
+                    <label for="times-a-week">Some times per week</label>
+                    <input type="radio" name="frequency" id="times-a-week" value="times-a-week">
+                    <label for="every-each-day">Every some day</label>
+                    <input type="radio" name="frequency" id="every-each-day" value="every-each-day">
+                </div>
+                <label for="startShowHour">Show from</label>
+                <input type="time" name="startShowHour" id="startShowHour">
+                <label for="endShowHour">to</label>
+                <input type="time" name="endShowHour" id="endShowHour">
+                <input type="submit" name="submit" value="test">
+            `
+
+            form.addEventListener('submit',testFormData);
+            dialogContent.append(form);
         }
 
-        function generatePositiveYN(){
-            return 0;
+        function generatePositiveYN(container){
+            container.innerHTML += `
+                <label for="title">Name</label>
+                <input type="text" name="title" id="title" placeholder="e.g. Exercise">
+                <label for="description">Description</label>
+                <input type="text" name="description" id="description" placeholder="e.g. Did I exercise today">
+            `
         }
 
-        function generateNegativeYN(){
+        function generateNegativeYN(container){
+            container.innerHTML += `
+                <label for="title">Name</label>
+                <input type="text" name="title" id="title" placeholder="e.g. Fast Food">
+                <label for="description">Description</label>
+                <input type="text" name="description" id="description" placeholder="e.g. Did I eat fast food today?">
+            `
+        }
+
+        function generatePositiveNumerical(container){
 
         }
 
-        function generatePositiveNumerical(){
-
-        }
-
-        function generateNegativeNumerical(){
+        function generateNegativeNumerical(container){
 
         }
 
@@ -80,10 +119,10 @@ const DOMController = (function(){
     })();
 
     function generateHabitDialog(dialogElement, event){
-        habitCreator.generateDialogContent(event.currentTarget.value);
+        habitGenerator.generateDialogContent(event.currentTarget.value);
     }
 
-    return {generateMobileHeader,createHabitCreatorDialog,habitPickerDialog};
+    return {generateMobileHeader,createHabitCreatorDialog,habitPickerDialog: createHabitPickerDialog};
 })();
 
 DOMController.generateMobileHeader();
